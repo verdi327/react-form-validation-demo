1906b9a87fb7f37ad8d1ce7961895faa72fdf8d2
Michael Verdi <michael.v.verdi@gmail.com>
2019-05-02 10:24:26 -0400

    update className error & add onChange for fields

--- src/App.js
+++ src/App.js
@@ -10,6 +10,22 @@ class App extends React.Component {
     formValid: false
   }
 
+  updateUsername = (username) => {
+    this.setState({username})
+  }
+
+  updateEmail = (email) => {
+    this.setState({email})
+  }
+
+  updatePassword = (password) => {
+    this.setState({password})
+  }
+
+  updatePasswordConfirm = (passwordConfirm) => {
+    this.setState({passwordConfirm})
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,26 +34,26 @@ class App extends React.Component {
         </header>
         <main role='main'>
           <form action='#' id='js-form'>
-            <div class='form-group'>
+            <div className='form-group'>
               <label htmlFor='username'>Username</label>
               <input type='text' id='username' name='username' className='form-field'
-              value={this.state.username}/>
+              value={this.state.username} onChange={(e) => this.updateUsername(e.target.value)}/>
             </div>
-            <div class='form-group'>
+            <div className='form-group'>
               <label htmlFor='email'>Email</label>
               <input type='email' id='email' name='email' className='form-field'
-              value={this.state.email}/>
+              value={this.state.email} onChange={(e) => this.updateEmail(e.target.value)}/>
             </div>
-            <div class='form-group'>
+            <div className='form-group'>
               <label htmlFor='password'>Password</label>
               <input type='password' id='password' name='password' className='form-field'
-              value={this.state.password}/>
+              value={this.state.password} onChange={(e) => this.updatePassword(e.target.value)}/>
             </div>
-            <div class='form-group'>
+            <div className='form-group'>
               <label htmlFor='password-confirmation'>Password Confirmation</label>
-              <input type='password' id='password-confirmation' name='password-confirmation' className='form-field' value={this.state.passwordConfirm}/>
+              <input type='password' id='password-confirmation' name='password-confirmation' className='form-field' value={this.state.passwordConfirm} onChange={(e) => this.updatePasswordConfirm(e.target.value)}/>
             </div>
-            <div class='form-controls'>
+            <div className='form-controls'>
               <button className='button' type='submit'>Sign Up</button>
             </div>
           </form>
