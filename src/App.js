576d1eb03a786b2d558e1b204b0020aed34efb3b
Michael Verdi <michael.v.verdi@gmail.com>
2019-05-02 10:00:12 -0400

    initial form markup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    formValid: false
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          Form Validation
+        </header>
+        <main role='main'>
+          <form action='#' id='js-form'>
+            <div class='form-group'>
+              <label htmlFor='username'>Username</label>
+              <input type='text' id='username' name='username' className='form-field'
+              value={this.state.username}/>
+            </div>
+            <div class='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input type='email' id='email' name='email' className='form-field'
+              value={this.state.email}/>
+            </div>
+            <div class='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input type='password' id='password' name='password' className='form-field'
+              value={this.state.password}/>
+            </div>
+            <div class='form-group'>
+              <label htmlFor='password-confirmation'>Password Confirmation</label>
+              <input type='password' id='password-confirmation' name='password-confirmation' className='form-field' value={this.state.passwordConfirm}/>
+            </div>
+            <div class='form-controls'>
+              <button className='button' type='submit'>Sign Up</button>
+            </div>
+          </form>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
