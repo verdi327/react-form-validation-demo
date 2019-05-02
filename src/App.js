b6994a9deac457d1e41ac65291dd27a7a5944cb0
Michael Verdi <michael.v.verdi@gmail.com>
2019-05-02 10:56:17 -0400

    added username validation func and ValidationError comp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import './App.css';
 
+function ValidationMessage(props) {
+  if (!props.valid) {
+    return(
+      <div className='error-msg'>{props.message}</div>
+    )
+  }
+  return null;
+}
+
 class App extends React.Component {
   state = {
-    username: '',
+    username: '', usernameValid: false,
     email: '',
     password: '',
     passwordConfirm: '',
-    formValid: false
+    formValid: false,
+    errorMsg: {}
+  }
+
+  validateForm = () => {
+    this.setState({
+      formValid: this.state.usernameValid
+    })
   }
 
   updateUsername = (username) => {
-    this.setState({username})
+    this.setState({username}, this.validateUsername)
+  }
+
+  validateUsername = () => {
+    // must be 3
+    // must have a number
+    const {username} = this.state;
+    let usernameValid = true;
+    let errorMsg = {...this.state.errorMsg}
+
+    if (username.length < 3) {
+      usernameValid = false;
+      errorMsg.username = 'Must be at least 3 characters long'
+    }
+
+    this.setState({usernameValid, errorMsg}, this.validateForm)
   }
 
   updateEmail = (email) => {
@@ -36,6 +67,7 @@ class App extends React.Component {
           <form action='#' id='js-form'>
             <div className='form-group'>
               <label htmlFor='username'>Username</label>
+              < ValidationMessage valid={this.state.usernameValid} message={this.state.errorMsg.username} />
               <input type='text' id='username' name='username' className='form-field'
               value={this.state.username} onChange={(e) => this.updateUsername(e.target.value)}/>
             </div>
@@ -54,7 +86,7 @@ class App extends React.Component {
               <input type='password' id='password-confirmation' name='password-confirmation' className='form-field' value={this.state.passwordConfirm} onChange={(e) => this.updatePasswordConfirm(e.target.value)}/>
             </div>
             <div className='form-controls'>
-              <button className='button' type='submit'>Sign Up</button>
+              <button className='button' type='submit' disabled={!this.state.formValid}>Sign Up</button>
             </div>
           </form>
         </main>
