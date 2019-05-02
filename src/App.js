da9f73828879527b139732e29966f410583de3db
Michael Verdi <michael.v.verdi@gmail.com>
2019-05-02 12:57:01 -0400

    added all field validations

--- src/App.js
+++ src/App.js
@@ -13,16 +13,17 @@ function ValidationMessage(props) {
 class App extends React.Component {
   state = {
     username: '', usernameValid: false,
-    email: '',
-    password: '',
-    passwordConfirm: '',
+    email: '', emailValid: false,
+    password: '', passwordValid: false,
+    passwordConfirm: '', passwordConfirmValid: false,
     formValid: false,
     errorMsg: {}
   }
 
   validateForm = () => {
+    const {usernameValid, emailValid} = this.state;
     this.setState({
-      formValid: this.state.usernameValid
+      formValid: usernameValid && emailValid
     })
   }
 
@@ -31,8 +32,6 @@ class App extends React.Component {
   }
 
   validateUsername = () => {
-    // must be 3
-    // must have a number
     const {username} = this.state;
     let usernameValid = true;
     let errorMsg = {...this.state.errorMsg}
@@ -46,15 +45,65 @@ class App extends React.Component {
   }
 
   updateEmail = (email) => {
-    this.setState({email})
+    this.setState({email}, this.validateEmail)
+  }
+
+  validateEmail = () => {
+    const {email} = this.state;
+    let emailValid = true;
+    let errorMsg = {...this.state.errorMsg}
+
+    // checks for format _@_._
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+      emailValid = false;
+      errorMsg.email = 'Invalid email format'
+    }
+
+    this.setState({emailValid, errorMsg}, this.validateForm)
   }
 
   updatePassword = (password) => {
-    this.setState({password})
+    this.setState({password}, this.validatePassword);
+  }
+
+  validatePassword = () => {
+    const {password} = this.state;
+    let passwordValid = true;
+    let errorMsg = {...this.state.errorMsg}
+
+    // must be 6 chars
+    // must contain a number
+    // must contain a special character
+
+    if (password.length < 6) {
+      passwordValid = false;
+      errorMsg.password = 'Password must be at least 6 characters long';
+    } else if (!/\d/.test(password)){
+      passwordValid = false;
+      errorMsg.password = 'Password must contain a digit';
+    } else if (!/[!@#$%^&*]/.test(password)){
+      passwordValid = false;
+      errorMsg.password = 'Password must contain special character: !@#$%^&*';
+    }
+
+    this.setState({passwordValid, errorMsg}, this.validateForm);
   }
 
   updatePasswordConfirm = (passwordConfirm) => {
-    this.setState({passwordConfirm})
+    this.setState({passwordConfirm}, this.validatePasswordConfirm)
+  }
+
+  validatePasswordConfirm = () => {
+    const {passwordConfirm, password} = this.state;
+    let passwordConfirmValid = true;
+    let errorMsg = {...this.state.errorMsg}
+
+    if (password !== passwordConfirm) {
+      passwordConfirmValid = false;
+      errorMsg.passwordConfirm = 'Passwords do not match'
+    }
+
+    this.setState({passwordConfirmValid, errorMsg}, this.validateForm);
   }
 
   render() {
@@ -73,16 +122,19 @@ class App extends React.Component {
             </div>
             <div className='form-group'>
               <label htmlFor='email'>Email</label>
+              < ValidationMessage valid={this.state.emailValid} message={this.state.errorMsg.email} />
               <input type='email' id='email' name='email' className='form-field'
               value={this.state.email} onChange={(e) => this.updateEmail(e.target.value)}/>
             </div>
             <div className='form-group'>
               <label htmlFor='password'>Password</label>
+              < ValidationMessage valid={this.state.passwordValid} message={this.state.errorMsg.password} />
               <input type='password' id='password' name='password' className='form-field'
               value={this.state.password} onChange={(e) => this.updatePassword(e.target.value)}/>
             </div>
             <div className='form-group'>
               <label htmlFor='password-confirmation'>Password Confirmation</label>
+              < ValidationMessage valid={this.state.passwordConfirmValid} message={this.state.errorMsg.passwordConfirm} />
               <input type='password' id='password-confirmation' name='password-confirmation' className='form-field' value={this.state.passwordConfirm} onChange={(e) => this.updatePasswordConfirm(e.target.value)}/>
             </div>
             <div className='form-controls'>
